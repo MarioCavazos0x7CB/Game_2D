45a69e55b7f76d108c0bff8f6debad452628b0cc
unknown <ma.cavazoscastillo@gmail.com>
2020-09-17 16:08:23 -0500

    First Commit

---
+++ winMain.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#pragma warning(push, 3)
+#include <Windows.h>
+#pragma warning (pop)
+
+int WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, PSTR CommandLine, INT CommandShow) 
+{
+	UNREFERENCED_PARAMETER(Instance);
+	UNREFERENCED_PARAMETER(PreviousInstance);
+	UNREFERENCED_PARAMETER(CommandLine);
+	UNREFERENCED_PARAMETER(CommandShow);
+
+	return (0);
+}
